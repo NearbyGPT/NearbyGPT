6680cdd156e539dc8600cb473105377317e6eff4
MuhammedMohsenn <muhammedmohsen8695@gmail.com>
2025-09-27 00:10:15 +0300

    mapbox setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Map from "@/components/Map";
+
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <h1 className="text-center text-4xl text-dark">Nearby GPT</h1>
+      {/* <h1 className="text-center text-4xl text-dark">Nearby GPT</h1> */}
+      <Map />
     </div>
   );
 }
---
+++ components/Map.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
+// Ensure the token is set
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
+
+export default function Map() {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    if (map.current) return;
+
+    if (!mapContainer.current) return;
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [29.9187, 31.2001],
+      zoom: 11,
+    });
+
+    map.current.addControl(new mapboxgl.NavigationControl());
+  }, []);
+
+  return <div ref={mapContainer} style={{ width: "100vw", height: "100vh" }} />;
+}
