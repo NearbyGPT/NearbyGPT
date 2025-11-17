b2e996be44d93eee43fd851d7808f55749814ae5
Metin Demir <metmirr@gmail.com>
2025-11-17 21:05:40 +0200

    feat: update mapbox style to minimalist

--- components/Map.tsx
+++ components/Map.tsx
@@ -308,7 +308,7 @@ export default function Map() {
 
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
+      style: 'mapbox://styles/mapbox/basic-v9',
       center: [29.9187, 31.2001],
       zoom: 15,
       attributionControl: false,
